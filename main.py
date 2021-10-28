0821e9cbeb9f4caa9132b0342e8847090e940861
Marcel Lobão <marcelblobao@gmail.com>
2021-10-28 12:45:29 -0300

    main

---
+++ main.py
@@ -0,0 +1,20 @@
+# Allow running from subfolder
+from sys import path
+from os import getcwd
+from os.path import join
+path.append(join(getcwd(), 'MLBlackJack'))
+#init class for handling output
+from consoleGui import Console
+
+# init file for BlackJack Game
+from blackjack import BlackJack
+
+if __name__ == '__main__':
+    console = Console(mode=Console.mode.DEFAULT_MODE)
+    
+    # TO DO : Show Menu options for player and mode inputs
+
+    with BlackJack('Tiago', players=7, console=console, mode=BlackJack.Mode.one_up) as game:
+        game.play()
+
+    console.print("Goodbye!")
